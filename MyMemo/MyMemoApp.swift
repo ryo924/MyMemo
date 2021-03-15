5c9130ede8a0dac890c0a4de411eb31a22676cc4
ryo924 <ryo.tominaga24@gmail.com>
2021-03-15 21:05:44 +0900

    Initial Commit

---
+++ MyMemo/MyMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMemoApp.swift
+//  MyMemo
+//
+//  Created by 冨永　涼 on 2021/03/15.
+//
+
+import SwiftUI
+
+@main
+struct MyMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
